2853756bfbbc39bbc6f6ef725fca2d61915b04d1
Eric Kryski <e.kryski@gmail.com>
2013-03-14 02:29:43 -0600

    started setting project and the landing page

---
+++ static-server.js
@@ -0,0 +1,25 @@
+var express = require("express"),
+    app     = express();
+    port    = parseInt(process.env.PORT, 10) || 8000;
+
+app.configure(function(){
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(express.static('public'));
+  app.use(express.static(__dirname));
+  app.use(express.errorHandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
+  app.use(express.logger({format: 'dev'}));
+  app.use(app.router);
+});
+
+app.get('*', function(req, res, next){
+  res.sendfile('./public/index.html');
+});
+
+if (!module.parent) {
+  app.listen(port);
+  console.log('Express static server started on 127.0.0.1:' + port);
+}
